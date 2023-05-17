296a06283767e2231d16ee4e232a6839776ca78f
John <johnfe@outlook.es>
2023-05-16 19:42:52 -0500

    taller 5

---
+++ src/main/java/com/example/demo/Materia.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+public class Materia {
+	private String nombre;
+	
+	
+	
+	
+	
+
+	@Override
+	public String toString() {
+		return "Materia [nombre=" + nombre + "]";
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+}
---
+++ src/test/java/com/example/demo/Pa2U1P4RaAqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pa2U1P4RaAqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
